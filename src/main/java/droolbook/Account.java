0dc358a940c45ee1d7c46a78a0823964b36bd14c
HP <hp@gpgpgp.fff>
2010-12-10 23:10:03 +0100

    xx

--- src/main/java/droolbook/Account.java
+++ src/main/java/droolbook/Account.java
@@ -2,30 +2,38 @@ package droolbook;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class Account {
-  private long balance;
+    private long balance;
 
-  public long getBalance() {
-    return balance;
-  }
+    public long getBalance() {
+        return balance;
+    }
 
-  public void setBalance(long balance) {
-    this.balance = balance;
-  }
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
 
-  @Override
-  public boolean equals(final Object other) {
-    if (this == other)
-      return true;
-    if (!(other instanceof Account))
-      return false;
-    Account castOther = (Account) other;
-    return new EqualsBuilder().append(balance, castOther.balance).isEquals();
-  }
+    @Override
+    public boolean equals(final Object other) {
+        if (this == other)
+            return true;
+        if (!(other instanceof Account))
+            return false;
+        Account castOther = (Account) other;
+        return new EqualsBuilder().append(balance, castOther.balance).isEquals();
+    }
 
-  @Override
-  public int hashCode() {
-    return new HashCodeBuilder(1450207409, -1692382659).append(balance).toHashCode();
-  }
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(1450207409, -1692382659).append(balance).toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).
+                append("balance", balance).
+                toString();
+    }
 }
