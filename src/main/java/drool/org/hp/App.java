38faa4d59160aa26389f3a52a34572b7f59225f0
HP <hp@gpgpgp.fff>
2010-12-20 22:35:00 +0100

    code format

--- src/main/java/drool/org/hp/App.java
+++ src/main/java/drool/org/hp/App.java
@@ -2,12 +2,10 @@ package drool.org.hp;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
