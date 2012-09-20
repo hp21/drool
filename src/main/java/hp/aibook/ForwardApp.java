c6212fc6b9cd25f5f958543c3bb4202eda555ae6
hp21 <hp21@gmail.com>
2012-09-20 22:06:55 +0200

    ai boo exampe added

---
+++ src/main/java/hp/aibook/ForwardApp.java
@@ -0,0 +1,45 @@
+package hp.aibook;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.joda.time.DateTime;
+
+public class ForwardApp {
+
+	public static final void main(String[] args) {
+		KnowledgeBase knowledgeBase = createKnowledgeBase();
+//		StatefulKnowledgeSession session = knowledgeBase.newStatefulKnowledgeSession();
+		StatefulKnowledgeSession session = knowledgeBase.newStatefulKnowledgeSession();
+		try {
+
+			session.insert(new A());
+			session.insert(new B());
+			session.insert(new C());
+			session.insert(new D());
+			session.insert(new E());
+
+			session.fireAllRules();
+
+		} finally {
+			session.dispose();
+		}
+	}
+
+	private static KnowledgeBase createKnowledgeBase() {
+		KnowledgeBuilder builder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		builder.add(ResourceFactory.newClassPathResource("hp/aibook/forward.drl"), ResourceType.DRL);
+		
+		if (builder.hasErrors()) {
+			throw new RuntimeException(builder.getErrors().toString());
+		}
+		
+		KnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
+		knowledgeBase.addKnowledgePackages(builder.getKnowledgePackages());
+		return knowledgeBase;
+	}
+}
